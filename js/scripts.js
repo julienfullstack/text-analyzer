61c65cf1724023d1e9a03eefa59289d1f076b6fb
Julien L <julienlenaz96@gmail.com>
2023-06-21 21:04:03 -0400

    Added function to count number of words in text regardless of case

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+// Business Logic
+
+
+function wordCounter(text) {
+  if (text.trim().length === 0) {
+    return 0;
+  }
+  let wordCount = 0;
+  const textArray = text.split(" ");
+  textArray.forEach(function(element) {
+    if (!Number(element)) {
+      wordCount++;
+    }
+  });
+  return wordCount;
+}
+
+function numberOfOccurrencesInText(word, text) {
+  const textArray = text.split(" ");
+  let wordCount = 0;
+  textArray.forEach(function(element) {
+    if (word.toLowerCase() === element.toLowerCase()) {
+      wordCount++;
+    }
+  });
+  return wordCount;
+}
